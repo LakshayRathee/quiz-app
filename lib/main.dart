b035ffd19a0a00c1b08d5f430d171509a0dd4c55
LakshayRathee <112965833+LakshayRathee@users.noreply.github.com>
2024-02-20 02:11:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: ,),));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
 // import 'package:quiz_app/gradiant_container.dart';
 
@@ -15,3 +33,4 @@ void main() {
 //     ),
 //   );
 // }
+
